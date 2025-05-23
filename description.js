d7eab79604a5fe13e5e22880184aa3aa9c17e777
dorisyin555 <Doris_Yin@avenues-ny.org>
2025-05-22 22:49:20 -0400

    Create description.js

---
+++ description.js
@@ -0,0 +1,5 @@
+Describe the things that you are modeling: ("I'll have a class for a multi-part pendulum which will manage Pendulum objects," "I'll have a Rabbit class and a Fox class," "I'll have a Cell class")
+Describe the other variables and data structures you'll need. 
+Decribe what you will do in setup.
+Describe how the flow of draw will work.
+Describe each of the classes you need in terms of properties and actions.
